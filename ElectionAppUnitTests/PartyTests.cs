14f6ad908d96d5215fece5bdb6ae4304a7006906
Achika213 <angel.tsvetanov.filipov@gmail.com>
2023-03-26 01:59:41 +0200

    Removed needless usings

--- ElectionAppUnitTests/PartyTests.cs
+++ ElectionAppUnitTests/PartyTests.cs
@@ -16,15 +16,12 @@ namespace ElectionAppUnitTests
 
         [TestMethod]
         public void TestConstructorWithListNumberAndName()
-        {
-            // Arrange
+        {            
             int expectedListNumber = 1;
             string expectedName = "Test Party";
-
-            // Act
+            
             Party party = new Party(expectedListNumber, expectedName);
-
-            // Assert
+            
             Assert.AreEqual(expectedListNumber, party.ListNumber);
             Assert.AreEqual(expectedName, party.Name);
             Assert.AreEqual(0, party.Votes);
@@ -32,16 +29,13 @@ namespace ElectionAppUnitTests
 
         [TestMethod]
         public void TestConstructorWithListNumberNameAndPreferenceNumber()
-        {
-            // Arrange
+        {            
             int expectedListNumber = 1;
             string expectedName = "Test Party";
             int expectedPreferenceNumber = 1;
-
-            // Act
+           
             Party party = new Party(expectedListNumber, expectedName, expectedPreferenceNumber);
-
-            // Assert
+            
             Assert.AreEqual(expectedListNumber, party.ListNumber);
             Assert.AreEqual(expectedName, party.Name);
             Assert.AreEqual(expectedPreferenceNumber, party.PreferenceNumber);
@@ -50,92 +44,62 @@ namespace ElectionAppUnitTests
 
         [TestMethod]
         public void TestIDSetterWithValidValue()
-        {
-            // Arrange
+        {            
             Party party = new Party();
-            int expectedID = 1;
-
-            // Act
-            party.ID = expectedID;
-
-            // Assert
+            int expectedID = 1;            
+            party.ID = expectedID;            
             Assert.AreEqual(expectedID, party.ID);
         }
 
         [TestMethod]
         public void TestIDSetterWithInvalidValue()
-        {
-            // Arrange
-            Party party = new Party();
-
-            // Assert
+        {           
+            Party party = new Party();            
             Assert.ThrowsException<ArgumentException>(() => party.ID = 0);
         }
 
         [TestMethod]
         public void TestNameSetterWithValidValue()
-        {
-            // Arrange
+        {            
             Party party = new Party();
-            string expectedName = "Test Party";
-
-            // Act
-            party.Name = expectedName;
-
-            // Assert
+            string expectedName = "Test Party";            
+            party.Name = expectedName;            
             Assert.AreEqual(expectedName, party.Name);
         }
 
         [TestMethod]
         public void TestNameSetterWithNullOrEmptyValue()
-        {
-            // Arrange
+        {            
             Party party = new Party();
-
-            // Assert
+            
             Assert.ThrowsException<ArgumentException>(() => party.Name = null);
             Assert.ThrowsException<ArgumentException>(() => party.Name = "");
         }
 
         [TestMethod]
         public void TestListNumberSetterWithValidValue()
-        {
-            // Arrange
+        {            
             Party party = new Party();
-            int expectedListNumber = 1;
-
-            // Act
-            party.ListNumber = expectedListNumber;
-
-            // Assert
+            int expectedListNumber = 1;            
+            party.ListNumber = expectedListNumber;            
             Assert.AreEqual(expectedListNumber, party.ListNumber);
         }
 
         [TestMethod]
         public void TestPreferenceNumberSetterWithValidValue()
-        {
-            // Arrange
+        {            
             Party party = new Party();
-            int expectedPreferenceNumber = 1;
-
-            // Act
-            party.PreferenceNumber = expectedPreferenceNumber;
-
-            // Assert
+            int expectedPreferenceNumber = 1;           
+            party.PreferenceNumber = expectedPreferenceNumber;            
             Assert.AreEqual(expectedPreferenceNumber, party.PreferenceNumber);
         }
 
         [TestMethod]
         public void TestVotesGetterAndSetter()
         {
-            // Arrange
             Party party = new Party();
-            int expectedVotes = 10;
-
-            // Act
-            party.Votes = expectedVotes;
-
-            // Assert
+            int expectedVotes = 10;            
+            party.Votes = expectedVotes;            
             Assert.AreEqual(expectedVotes, party.Votes);
         }
     }
